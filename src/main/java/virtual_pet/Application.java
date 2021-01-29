700c44204c6676ef583c709231c1ce3fdd129354
Celia Nziadeu <Clingobayo@gmail.com>
2021-01-29 13:58:33 -0500

    updated virtual-pet-java-scripted code

--- src/main/java/virtual_pet/Application.java
+++ src/main/java/virtual_pet/Application.java
@@ -3,13 +3,23 @@ package virtual_pet;
 import java.util.Scanner;
 
 public class Application {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean isGameRunning = true;
         VirtualPet pet = intro(input);
-        instructions(pet);
+
+        while (isGameRunning){
+
+            instructions(pet);
+            commands(pet, input);
+            pet.tick();
+            
+        }
+
 
     }
-    public static VirtualPet intro(Scanner input){
+
+    public static VirtualPet intro(Scanner input) {
         System.out.println("Welcome to the Java scripters virtual pet game!");
         System.out.println("What would you like to name your pet?");
         String petName = input.nextLine();
@@ -17,9 +27,10 @@ public class Application {
         System.out.println("Say hello to your new pet " + pet.getName() + "!");
         return pet;
     }
-    public static void instructions(VirtualPet pet){
+
+    public static void instructions(VirtualPet pet) {
         System.out.println("To interact with " + pet.getName() + ", enter '1' to feed "
-                        + pet.getName() + ".");
+                + pet.getName() + ".");
         System.out.println("Enter '2' to give your pet a bath.");
         System.out.println("Enter '3' to give your pet water.");
         System.out.println("Enter '4' to play with your pet.");
@@ -27,4 +38,39 @@ public class Application {
         System.out.println("and if your would like to leave, enter '0'.");
     }
 
-}
+
+
+    public static void commands(VirtualPet pet, Scanner input) {
+        int command = input.nextInt();
+        switch (command) {
+            case 0:
+                System.exit(0);
+            case 1:
+                pet.eat();
+                break;
+            case 2:
+                pet.bath();
+                break;
+            case 3:
+                pet.drink();
+                break;
+            case 4:
+                pet.play();
+                break;
+            case 5:
+                pet.status();
+                instructions(pet);
+                commands(pet, input);
+                break;
+
+            default :
+                System.out.println("Not a valid instruction, please enter a new command");
+
+                instructions(pet);
+                commands(pet, input);
+
+        }
+
+
+        }
+    }
--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -62,6 +62,7 @@ public class VirtualPet {
         }
     }
 
+
     public void tick() {
         health -= 10;
         hunger += 10;
@@ -69,6 +70,11 @@ public class VirtualPet {
         boredom += 10;
         isDeadCheck();
     }
+
+//Altenative code
+// hunger-= 30;
+// if(hunger<0){
+// hunger=0;
 
 
     public void eat() {
@@ -106,5 +112,47 @@ public class VirtualPet {
 
     }
 
+    public void status() {
+        if (health <= 100 && health >= 80) {
+            System.out.println(name + " is feeling healthy");
+        } else if (health < 80 && health >= 50) {
+            System.out.println(name + " is feeling okay");
+        } else if (health < 50 && health >= 30) {
+            System.out.println(name + " is feeling sick");
+        } else {
+            System.out.println(name + " need medical attention");
+        }
+        if (boredom >= 80) {
+            System.out.println(name + " is very bored at home");
+        } else if (boredom < 80 && boredom >= 50) {
+            System.out.println(name + " wants to go outside");
+        } else if (boredom < 50 && boredom >= 30) {
+            System.out.println(name + " enjoys playing with you");
+        } else {
+            System.out.println(name + " is feeling super excited");
+        }
+        if (hunger >= 80) {
+            System.out.println(name + " is starving");
+        } else if (hunger < 80 && hunger >= 50) {
+            System.out.println(name + " is hungry");
+        } else if (hunger < 50 && hunger >= 30) {
+            System.out.println(name + " is not very hungry");
+        } else {
+            System.out.println(name + " is full");
+        }
+        if (thirst >= 80) {
+            System.out.println(name + " is very dehydrated");
+        } else if (thirst < 80 && thirst >= 50) {
+            System.out.println(name + " is thirsty");
+        } else if (thirst < 50 && thirst >= 30) {
+            System.out.println(name + " is not very thirsty");
+        } else {
+            System.out.println(name + " is hydrated");
 
+
+        }
+    }
 }
+
+
+
